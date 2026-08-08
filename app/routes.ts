571c91fa6c4ed341b74d41abd4652ea24ec0abab
panahpiruncharoen <panahpirun@gmail.com>
2026-08-08 15:37:13 +0700

    added pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import { type RouteConfig, index, route} from "@react-router/dev/routes";
+export default [
+    index("routes/home.tsx"), 
+    route("login", "routes/login.tsx"),
+    route("settings", "routes/settings.tsx"),
+    route("profile", "routes/profile.tsx"),
+    route("exchanges", "routes/exchanges.tsx"),
+    route("chat", "routes/chat.tsx"),
+    route("history", "routes/history.tsx"),
+] satisfies RouteConfig;
