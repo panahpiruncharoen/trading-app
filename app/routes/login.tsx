c4b0454df4848627b5649547502a18eab088059d
Panyut Sriwirote <mildpanyut@gmail.com>
2026-09-05 16:06:41 +0700

    add login action

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -13,6 +13,7 @@ import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
 import { auth } from "~/lib/firebase"
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { redirect, Form } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -21,11 +22,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-async function login()
-{
-  await signInWithEmailAndPassword(auth, "admin@tradingapp.com", "123456")
+export async function clientAction({
+  request,
+}: Route.ClientActionArgs) {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  if (!email || !password) {
+    return "Missing credentials"
+  }
+  try {
+    await signInWithEmailAndPassword(auth, String(email), String(password))
+  } catch {
+    return "Invalid credentials"
+  }
+  return redirect("/");
 }
-export default function Home() {
+
+export default function Home({ actionData }) {
   return (
     <div className="items-center justify-center w-full min-h-screen flex">
       <Card className="w-full max-w-sm">
@@ -35,7 +49,7 @@ export default function Home() {
           Enter your email below to login to your account
         </CardDescription>
       </CardHeader>
-      <form>
+      <Form method="post">
       <CardContent>      
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
@@ -43,6 +57,7 @@ export default function Home() {
               <Input
                 id="email"
                 type="email"
+                name="email"
                 placeholder="m@example.com"
                 required
               />
@@ -51,16 +66,17 @@ export default function Home() {
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
               </div>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" name="password" required />
             </div>
           </div>        
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full" onClick={login}>
+        <Button type="submit" className="w-full">
           Login
         </Button>
+        {actionData}
       </CardFooter>
-      </form>
+      </Form>
     </Card>
     </div>
     
